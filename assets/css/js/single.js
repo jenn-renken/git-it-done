e816fbd2afa70539bf1e2769fa52356cf947cb48
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2021-09-23 12:18:21 -0400

    added error handling

--- assets/css/js/single.js
+++ assets/css/js/single.js
@@ -6,8 +6,13 @@ var repoNameEl = document.querySelector("#repo-name");
 var getRepoName = function() {
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
+
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
 }
 
 
@@ -29,8 +34,7 @@ var getRepoIssues = function(repo) {
       });
     }
     else {
-      console.log(response);
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
