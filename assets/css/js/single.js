07f64c608893be1e35ebed67ed330ccfb3faa76e
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2021-09-23 11:38:27 -0400

    added dynamic issue list and connected pages

--- assets/css/js/single.js
+++ assets/css/js/single.js
@@ -1,6 +1,15 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    getRepoIssues(repoName);
+    repoNameEl.textContent = repoName;
+}
+
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -81,4 +90,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
